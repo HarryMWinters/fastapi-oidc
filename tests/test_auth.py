3f90529382ef6332fe0ff78aa92c9c7112ba89ef
HarryMWinters <harrymcwinters@gmail.com>
2021-07-17 16:52:45 -0700

    Fix tests
    - Fix broken key generation in auth tests.
    - Update pre-commit.
    - Configure linting and analysis.
    - Update dependencies.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,33 +4,32 @@ import time
 import uuid
 
 import jwt
-from cryptography.hazmat.backends import default_backend as crypto_default_backend
-from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 from fastapi_oidc import auth
 
 
 class Fixtures:
-    _fixtures_directory = os.path.join(os.path.dirname(__file__), f"fixtures")
+    _fixtures_directory = os.path.join(os.path.dirname(__file__), "fixtures")
 
     with open(_fixtures_directory + "/AuthServerDiscovery.json") as f:
         OIDC_DISCOVERY_RESPONSE = json.load(f)
 
     _key = rsa.generate_private_key(
-        backend=crypto_default_backend(), public_exponent=65537, key_size=2048
+        backend=default_backend(), public_exponent=65537, key_size=2048
     )
+
     TESTING_PRIVATE_KEY = _key.private_bytes(
-        crypto_serialization.Encoding.PEM,
-        crypto_serialization.PrivateFormat.PKCS8,
-        crypto_serialization.NoEncryption(),
-    )
+        serialization.Encoding.PEM,
+        serialization.PrivateFormat.PKCS8,
+        serialization.NoEncryption(),
+    ).decode("UTF-8")
+
     TESTING_PUBLIC_KEY = (
         _key.public_key()
-        .public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH,
-        )
+        .public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.PKCS1)
         .decode("UTF-8")
     )
 
@@ -45,9 +44,9 @@ TEST_CONFIG = {
 
 def _make_token(
     email: str,
-    private_key=Fixtures.TESTING_PRIVATE_KEY,
-    client_id=TEST_CONFIG["client_id"],
-    issuer=TEST_CONFIG["issuer"],
+    private_key: str = Fixtures.TESTING_PRIVATE_KEY,
+    client_id: str = str(TEST_CONFIG["client_id"]),
+    issuer: str = str(TEST_CONFIG["issuer"]),
 ) -> str:
     now = int(time.time())
     return jwt.encode(
